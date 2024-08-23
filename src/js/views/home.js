3d4de7fa7adb20ea75fcbe92d5a471e67887c616
Cesar Contreras <cesar.alberto2003@outlook.com>
2024-08-23 16:25:47 +0000

    Data rendering

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -7,11 +7,19 @@ import { Context } from "../store/appContext";
 
 export const Home = () => {
 
-	const {store} = useContext(Context)
+	const { store } = useContext(Context)
 
 	return <div className="text-center mt-5">
 		<h1>Hello Rigo!</h1>
-		{store.characters.map(item => <h1>{item.name}</h1>)}
+		{store.characters.map(item => <div className="card" style={{ width: "18rem" }}>
+			<img src={`https://starwars-visualguide.com/assets/img/characters/${item.uid}.jpg`} className="card-img-top" alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">{item.name}</h5>
+				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" className="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
+		)}
 
 	</div>
 
